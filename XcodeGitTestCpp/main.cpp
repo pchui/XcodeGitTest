6cd0f2094a4739819f987af4fdca37b96ad8d4ea
Paul Chui <pchui@yahoo.com>
2016-09-15 12:11:43 -0700

    Initial Commit

---
+++ XcodeGitTestCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XcodeGitTestCpp
+//
+//  Created by Paul Chui on 9/15/16.
+//  Copyright © 2016 Company. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
